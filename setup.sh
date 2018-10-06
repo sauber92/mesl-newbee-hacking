f685e648f569ebd3f7cdc2a85b669a920a9d639a
root <root@localhost.localdomain>
2018-10-05 20:03:56 -0400

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cp Practice1/web/index.html /var/www/html/
 cp -r Practice1/web/css/ /var/www/html/
 cp -r Practice1/web/img/ /var/www/html/
 cp -r Practice1/malware/introduce.pdf /var/www/html/
+cp Practice3/webshell.php /var/www/html/
 
 echo start apache server
 service apache2 start
