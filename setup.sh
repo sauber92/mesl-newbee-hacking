0ed475496e3594cd8c64347c1e7e21698f30f016
root <root@localhost.localdomain>
2018-10-05 20:28:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo Copy practice files
+echo Copy practice files...
 cp Practice1/web/index.html /var/www/html/
 cp -r Practice1/web/css/ /var/www/html/
 cp -r Practice1/web/img/ /var/www/html/
 cp -r Practice1/malware/introduce.pdf /var/www/html/
 cp Practice3/webshell.php /var/www/html/
+chmod 777 /var/www/html/webshell.php
 
-echo start apache server
+echo start apache server...
 service apache2 start
+
+echo Install program...
+apt install hexedit
